24c4c06491af7283bac28e543312d99a1bcb1cf5
Denis Simakov <denis.simakov@gmail.com>
2018-07-06 23:40:03 +0300

    message about registry - reason for update ignoring changes

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 sudo docker build -f Dockerfile.simple-flask -t hackathon2018:simple-flask .
 sudo docker service create --name simple-flask --publish published=5000,target=5000,mode=host --detach hackathon2018:simple-flask
 sudo docker build -f Dockerfile.simple-flask -t hackathon2018:simple-flask-2 .
+#image hackathon2018:simple-flask-2 could not be accessed on a registry to record
+#its digest. Each node will access hackathon2018:simple-flask-2 independently,
+#possibly leading to different nodes running different
+#versions of the image.
 sudo docker service update simple-flask --image hackathon2018:simple-flask-2
