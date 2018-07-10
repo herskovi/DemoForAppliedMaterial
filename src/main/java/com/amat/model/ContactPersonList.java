80b405161b6bd028957f44e44fc50e79f291e663
herskovi <herskovi77@gmail.com>
2018-07-10 14:28:59 +0300

    new version for hackathon applied

---
+++ src/main/java/com/amat/model/ContactPersonList.java
@@ -0,0 +1,16 @@
+package com.amat.model;
+
+import java.util.List;
+
+public class ContactPersonList {
+
+    private List<ContactPerson> contactPersonList = null;
+
+    public List<ContactPerson> getContactPersonList() {
+        return contactPersonList;
+    }
+
+    public void setContactPersonList(List<ContactPerson> contactPersonList) {
+        this.contactPersonList = contactPersonList;
+    }
+}
