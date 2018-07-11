f0a5e459b37bf0d264aa36d5b5fbd2a50b05e205
Denis Simakov <denis.simakov@gmail.com>
2018-07-11 08:21:43 +0300

    docker-infa - better html formatting

--- src/docker-infra/swarm-api.py
+++ src/docker-infra/swarm-api.py
@@ -2,6 +2,13 @@ import flask
 from flask_restful import Resource, Api, reqparse
 import docker
 docker_client = docker.from_env()
+import json
+
+def output_html(data, code, headers=None):
+    data = data.replace('\n', '<br/>\n').replace(' ', '&nbsp;')
+    resp = flask.Response(data, mimetype='text/html', headers=headers)
+    resp.status_code = code
+    return resp
 
 class CmdInfo(Resource):
 
@@ -14,12 +21,24 @@ class CmdInfo(Resource):
     def get(self):
         args = self.parser.parse_args()
         info = docker_client.info()
-        return flask.jsonify(info)
+        return output_html(json.dumps(info, indent=4), 200)
 
+class CmdTest(Resource):
+
+    name = 'test'
+
+    def __init__(self):
+        parser = reqparse.RequestParser()
+        self.parser = parser
+
+    def get(self):
+        args = self.parser.parse_args()
+        s = 'test1\ntest2\n'
+        return output_html(s, 200)
 
 class MyApp:
 
-    all_commands = [CmdInfo]
+    all_commands = [CmdInfo, CmdTest]
 
     def __init__(self):
         app = flask.Flask('swarm-api')
